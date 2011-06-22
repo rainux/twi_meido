2b5bf2e7148880ad03310682b2707cf244651356
Jimmy Xu <xu.jimmy.wrk@gmail.com>
2011-06-22 03:18:12 +0000

    Revert "Shorten any URL found if status is longer than 140, ご主人様.", ご主人様.
    This reverts commit c45042ef11fc2b1d8f3b5540b0a8b486bba0bc6f.
    
    Conflicts:
    
    	commands/tweet.rb

--- commands/tweet.rb
+++ commands/tweet.rb
@@ -3,44 +3,7 @@ module TwiMeido
     extend Command
 
     define_command :tweet, nil do |user, message|
-      text = message.strip
-      length = ActiveSupport::Multibyte::Chars.new(text).normalize(:c).length
-
-      # FIXME: Place this in a class and freeze it.
-      # Stolen from Twitter::Regex
-      url_regex = /(((?i-mx:[^-\/"':!=A-Z0-9_@\357\274\240]|^|\:))
-        (
-          (https?:\/\/)
-          ((?i-mx:(?-mix:(?:[^[:punct:]\s](?:[_-]|[^[:punct:]\s])*)?[^[:punct:]\s]\.)*(?-mix:(?:[^[:punct:]\s](?:[-]|[^[:punct:]\s])*)?[^[:punct:]\s])\.[a-z]{2,}(?::[0-9]+)?))
-          (\/
-            (?:
-              (?ix-m:
-      (?i-mx:\((?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\))|
-      @(?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\/|
-      [\.,](?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+|
-      (?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+
-    )+(?i-mx:[a-z0-9=_#\/\+\-]|(?i-mx:\((?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\)))|
-              (?ix-m:
-      (?i-mx:\((?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\))|
-      @(?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\/|
-      [\.,](?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+|
-      (?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+
-    )+(?i-mx:[a-z0-9=_#\/\+\-]|(?i-mx:\((?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\)))?|
-              (?i-mx:[a-z0-9=_#\/\+\-]|(?i-mx:\((?i-mx:[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~|\.])+\)))
-            )?
-          )?
-          (\?(?i-mx:[a-z0-9!\*'\(\);:&=\+\$\/%#\[\]\-_\.,~|])*(?i-mx:[a-z0-9_&=#\/]))?
-        )
-      )
-    /ix
-
-      if length > 140
-        text.gsub(url_regex) do
-          $2 + Googl.shorten($3).short_url
-        end
-      end
-
-      TwiMeido.current_user.update_status! :status => text
+      TwiMeido.current_user.update_status! :status => message.strip
       'Successfully tweeted, ご主人様.'
     end
   end
